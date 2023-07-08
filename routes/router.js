a2d9d2a91612d4594e604141de9e764a2d28cf4c
fandi-ahmad <fandi4160@gmail.com>
2023-07-08 19:21:58 +0800

    modified routes for render view

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const pegawaiRouter = require('./pegawaiRouter')
 const wargaRouter = require('./wargaRouter')
 const { getAllPegawai } = require('../controllers/pegawaiControllers')
 const { getAllWarga } = require('../controllers/wargaControllers')
-const { getAllSurat } = require('../controllers/suratControllers')
+const { getAllSurat, createSurat, updateSurat } = require('../controllers/suratControllers')
 
 router.use('/api/v1', pegawaiRouter)
 router.use('/api/v1', wargaRouter)
@@ -15,5 +15,7 @@ router.get('/pegawai', getAllPegawai)
 router.get('/warga', getAllWarga)
 
 router.get('/surat', getAllSurat)
+router.post('/api/v1/surat', createSurat)
+router.post('/api/v1/surat/update', updateSurat)
 
 module.exports = router
--- routes/wargaRouter.js
+++ routes/wargaRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
-const { getAllWarga, createWarga, deleteWarga } = require('../controllers/wargaControllers')
+const { getAllWarga, createWarga, deleteWarga, createWargaPelapor } = require('../controllers/wargaControllers')
 
 router.get('/warga', getAllWarga)
 router.post('/warga', createWarga)
+router.post('/warga/pelapor', createWargaPelapor)
 router.delete('/warga/:id', deleteWarga)
 
 module.exports = router
