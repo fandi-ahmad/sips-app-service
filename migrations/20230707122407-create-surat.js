7a61491f727acb45505680a1b27bf23773d9d44c
fandi-ahmad <fandi4160@gmail.com>
2023-07-08 10:39:58 +0800

    add table database and migrations

---
+++ migrations/20230707122407-create-surat.js
@@ -0,0 +1,66 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Surats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      no_surat: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nama_surat: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      maksud: {
+        type: Sequelize.STRING
+      },
+      id_pegawai: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Pegawais',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      id_warga: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Wargas',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      id_warga_pelapor: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Warga_pelapors',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Surats');
+  }
+};
