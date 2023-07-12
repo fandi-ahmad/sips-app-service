a7a0168afef784d7946fdca45809e01c51a79089
fandi-ahmad <fandi4160@gmail.com>
2023-07-12 11:01:25 +0800

    add .env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = () => { return process.env.APP_PORT }
 const router = require('./routes/router')
 const BodyParser = require("body-parser")
+const { config } = require('dotenv')
 
+config()
 app.use(BodyParser.urlencoded({ extended: true }))
 app.use(express.static('client'));
 app.set('view engine', 'ejs')
@@ -12,6 +14,6 @@ app.set('views', 'client/views')
 app.use(express.json())
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port(), () => {
+  console.log(`Example app listening on port ${port()}`)
 })
