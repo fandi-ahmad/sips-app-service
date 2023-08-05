673e7e76ec5a39715dd762ae6bb1d40e17adaaa6
fandi-ahmad <fandi4160@gmail.com>
2023-08-05 22:24:58 +0800

    add auth for access api

--- routes/pegawaiRouter.js
+++ routes/pegawaiRouter.js
@@ -2,10 +2,10 @@ const router = require("express").Router()
 const { createPegawai, updatePegawai, deletePegawai, getAllPegawai, getPegawaiActive } = require('../controllers/pegawaiControllers')
 const { verifyToken } = require('../middleware/VerifyToken')
 
-router.get('/pegawai', getAllPegawai)
-router.get('/pegawai/active', getPegawaiActive)
-router.post('/pegawai', createPegawai)
-router.put('/pegawai/update', updatePegawai)
-router.delete('/pegawai/delete/:id', deletePegawai)
+router.get('/pegawai', verifyToken, getAllPegawai)
+router.get('/pegawai/active', verifyToken, getPegawaiActive)
+router.post('/pegawai', verifyToken, createPegawai)
+router.put('/pegawai/update', verifyToken, updatePegawai)
+router.delete('/pegawai/delete/:id', verifyToken, deletePegawai)
 
 module.exports = router
